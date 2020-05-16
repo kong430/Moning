19558aea4801d7ba4caa963675a96643c19fbfb4
JaneYi <janeyi@naver.com>
2020-05-16 15:39:36 +0900

    git ignore

---
+++ Moning/Moning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moning
+//
+//  Created by 이제인 on 2020/05/16.
+//  Copyright © 2020 이제인. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
