47943f565577020bbcbbbb7600baf73d31748ace
JaneYi <janeyi@naver.com>
2020-05-28 15:15:14 +0900

    현위치 받아오기 #3
    위치 설정 안 하면 위치 좌표 nil 이라서 안 나옴
    설정 하면 바로 라벨에 나와야되는데 한 번 더 눌러야 나오는 문제 있음

---
+++ Moning/Data/PlaceData.swift
@@ -0,0 +1,18 @@
+//
+//  PlaceData.swift
+//  Moning
+//
+//  Created by 이제인 on 2020/05/25.
+//  Copyright © 2020 이제인. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class Place {
+    static var name: String! // 주소
+    static var lat: CLLocationDegrees! // 위도
+    static var lon: CLLocationDegrees! // 경도
+}
+
+
